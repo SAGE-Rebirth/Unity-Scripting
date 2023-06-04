9adbce8c4d04ed0ce0cab5ff707e1e8811a29d96
SAGE-Rebirth <abhinavs1711@gmail.com>
2023-06-04 22:01:48 +0530

    buller script added

---
+++ bullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<EnemyController>())
+            other.GetComponent<EnemyController>().Death();
+    }
+}
