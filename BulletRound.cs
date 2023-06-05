b8d9e5f60537e85ef2a9babc8dff0d75ef39da8c
SAGE-Rebirth <abhinavs1711@gmail.com>
2023-06-05 09:09:49 +0530

    BulletRound added

---
+++ BulletRound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BulletRound : MonoBehaviour
+{
+    public ParticleSystem hitEffect;
+    public int targetObjectLayer;
+    public int damage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == targetObjectLayer)
+        {
+            collision.gameObject.GetComponent<Health>().Damage(damage);
+        }
+
+        hitEffect.gameObject.SetActive(true);
+        hitEffect.transform.SetParent(null);
+
+        Destroy(gameObject);
+    }
+}
