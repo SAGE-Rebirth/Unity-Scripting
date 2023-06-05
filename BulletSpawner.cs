4e9b7748d009bdcadc1f8b8f0d9ca66fec33084e
SAGE-Rebirth <abhinavs1711@gmail.com>
2023-06-05 09:10:18 +0530

    BulletSpawner added

---
+++ BulletSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public float speed = 1;
+
+    public GameObject bullet;
+   
+   
+    public void ShootBullet()
+    {
+        var bulletInstance = Instantiate(bullet);
+        bulletInstance.transform.position = transform.position;
+        bulletInstance.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
